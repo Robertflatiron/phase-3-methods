2aa06875c77747d0054ac985c9fec34bd0d15a7d
Robert Gaspar <gaspar.robert7@gmail.com>
2021-12-14 14:04:24 -0600

    commit tree 53458ff973f990c4a6925af165cc9554435cc160

--- methods.rb
+++ methods.rb
@@ -1,1 +1,51 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+# greet_programmer
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+# greet
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+
+# greet_with_default("Naureen")
+# greet_with_default
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+sum = add(2, 5)
+
+def halve num
+    if num.class != Integer
+        return nil
+    else 
+        num / 2 
+    end 
+end
+
+# all three work, top and both on bottom
+
+# def halve num
+#     return nil unless num.class == Integer
+#     num/2
+# end
+
+# def halve num
+#     if num.class == Integer
+#         num/2
+#     else 
+#         nil
+# end
+
+
+
